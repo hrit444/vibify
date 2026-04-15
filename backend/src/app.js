84f8e96061bb2b03a7b5baac741510ca50cdb2cf
Hritam Samanta <hritam04.samanta@gmail.com>
2026-04-15 11:54:17 +0530

    route fixing in app

--- backend/src/app.js
+++ backend/src/app.js
@@ -8,10 +8,10 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 app.use('/api', songRoutes)
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, '../../frontend/dist')))
 
-app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/index.html'))
+app.get(/.*/, (req, res) => {
+    res.sendFile(path.join(__dirname, '../../frontend/dist/index.html'))
 })
 
 module.exports = app
